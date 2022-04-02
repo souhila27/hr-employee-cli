4d076d8f11aff2a461e06f33fcc6c147cac1354b
souhila27 <suhamimisou14@gmail.com>
2022-04-01 22:18:24 -0400

    added seed file

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO departments (name)
+VALUES
+  ('Electronics');
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+  ('Manager', 50000, 1),
+  ('Clerk', 35000, 1);
+
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+  ('James', 'Fraser', 1, 1),
+  ('James', 'Fraser', 1, 1);
